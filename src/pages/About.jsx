3ee2cbe5a95d65ed22f3a5931cf051484162cc98
usman <uz391110@gmail.com>
2022-03-24 15:59:57 +0500

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. This
+        project is developed by
+        
+          
+        
+        <strong>
+          <a href='https://pk.linkedin.com/in/usman-zahid-7a6228183?trk=people-guest_people_search-card'> Usman Zahid</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400'>
+        Github <span className='text-white'>usmanzahid389</span>
+      </p>
+      
+    </>
+  )
+}
+
+export default About
